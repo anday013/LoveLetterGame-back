0b3cd027e13333d496cb9f94fdf36fef6ddc753e
Anday <48630069+anday2000@users.noreply.github.com>
2020-05-15 19:01:16 +0400

    socket update

--- src/routes/sockets/roomSckt.js
+++ src/routes/sockets/roomSckt.js
@@ -59,8 +59,8 @@ module.exports = roomSckt = (io, socket, currentPlayer) => {
             switch (moveResult) {
                 case "Success":
                     moveResponse = new Response(moveResult, 200);
-                    let winner;
-                    if ((winner = gameFunctions.checkForWiner(currentGame))) {
+                    let winner = gameFunctions.checkForWinner(currentGame);
+                    if (winner) {
                         io.to(currentGame.room.name).emit('win', new Response("Win", 200, winner));
                         gameFunctions.sendPlayersWithoutCards(currentGame.activePlayers, io, currentGame);
                         currentGame = gameFunctions.newRound(currentGame, io);
