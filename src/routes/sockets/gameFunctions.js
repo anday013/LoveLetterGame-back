e967f2a746311cdb65073be4e9769fab68118545
Anday <48630069+anday2000@users.noreply.github.com>
2020-05-18 15:00:54 +0400

    game check

--- src/routes/sockets/gameFunctions.js
+++ src/routes/sockets/gameFunctions.js
@@ -32,7 +32,7 @@ function newRound(game, io) {
     game.players.forEach(p => p.reset());
     game.activePlayers = game.players.slice();
     initialActions(game, io);
-    let winner = game.isGameEnd();
+    let winner = game.checkForWinner();
     if(winner)
         io.to(game.room.name).emit('game-end', new Response("Game end", 200, winner))
     
