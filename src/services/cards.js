29c80bc87d73d9bbcbe7f3b80d85f4b941cfe53a
Ramin Afandiyev <41924783+raminafandi@users.noreply.github.com>
2020-05-23 06:40:42 +0400

    Bug fixed.

--- src/services/cards.js
+++ src/services/cards.js
@@ -1,53 +1,53 @@
 const deck = require('./deck');
 const score = require('./score');
 /*
-* If guessed card is exist in target player returns card, otherwise null
-*/
+ * If guessed card is exist in target player returns card, otherwise null
+ */
 function guard(targetPlayer, guessedCardPower, game) {
-    if (targetPlayer.cards.find(c => c.power == guessedCardPower)) {
-        return score.loose(game, targetPlayer);
-    }
-    return null;
+  if (targetPlayer.cards.find((c) => c.power == guessedCardPower)) {
+    return score.loose(game, targetPlayer);
+  }
+  return null;
 }
 
 /*
  * Returns target player cards
  */
 function priest(targetPlayer) {
-    return targetPlayer.cards.slice();
+  return targetPlayer.cards.slice();
 }
 
 /*
-* If current player has a stronger hand returns true, otherwise false
-*/
+ * If current player has a stronger hand returns true, otherwise false
+ */
 function baron(targetPlayer, currentPlayer, game) {
-    let targetPlayerPoints = 0, currentPlayerPoints = 0;
-    targetPlayer.cards.forEach(element => {
-        targetPlayerPoints += element.power;
-    });
-    currentPlayer.cards.forEach(element => {
-        currentPlayerPoints += element.power;
-    });
-    if (currentPlayerPoints > targetPlayerPoints) {
-        return score.loose(game, targetPlayer);
-    }
-    else if (currentPlayerPoints < targetPlayerPoints) {
-        return score.loose(game, currentPlayer);
-    }
-    return null;
+  let targetPlayerPoints = 0,
+    currentPlayerPoints = 0;
+  targetPlayer.cards.forEach((element) => {
+    targetPlayerPoints += element.power;
+  });
+  currentPlayer.cards.forEach((element) => {
+    currentPlayerPoints += element.power;
+  });
+  if (currentPlayerPoints > targetPlayerPoints) {
+    return score.loose(game, targetPlayer);
+  } else if (currentPlayerPoints < targetPlayerPoints) {
+    return score.loose(game, currentPlayer);
+  }
+  return null;
 }
 
 /*
-*  Player cannot be affected by any other player's card until the next turn.
-*/
+ *  Player cannot be affected by any other player's card until the next turn.
+ */
 function handmaid(currentPlayer, game) {
-    try {
-        game.activePlayers.find(p => p.id === currentPlayer.id).protected = true;
-        return true;
-    } catch (err) {
-        console.error(err);
-        return false;
-    }
+  try {
+    game.activePlayers.find((p) => p.id === currentPlayer.id).protected = true;
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 }
 
 function prince(targetPlayer, game) {
@@ -64,70 +64,58 @@ function prince(targetPlayer, game) {
           .addCard(game.reservedCard.setPlayerId(targetPlayer.id));
     }
 
-function prince(targetPlayer, game) {
-    try {
-        if (discardHand(targetPlayer, game)) {
-            let drawCard = deck.drawCardFromDeck(game.cardDeck);
-            if (drawCard)
-                game.activePlayers.find(p => p.id === targetPlayer.id).addCard(drawCard.setPlayerId(targetPlayer.id));
-            else
-                game.activePlayers.find(p => p.id === targetPlayer.id).addCard(game.reservedCard.setPlayerId(targetPlayer.id));
-        }
-
-        return targetPlayer.id;
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
+    return targetPlayer.id;
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
 }
-
 function discardHand(targetPlayer, game) {
-    try {
-        targetPlayer.cards.forEach(card => {
-            if (card.power === 8) {
-                princess(targetPlayer, game);
-                return false;
-            }
-        });
-        game.activePlayers.find(p => p.id === targetPlayer.id).cards.forEach(c => {
-            game.activePlayers.find(p => p.id === targetPlayer.id).discardedCards.push(c);
-        });
-        game.activePlayers.find(p => p.id === targetPlayer.id).cards = [];
-        return true;
-
-    } catch (error) {
-        console.error(error);
+  try {
+    targetPlayer.cards.forEach((card) => {
+      if (card.power === 8) {
+        princess(targetPlayer, game);
         return false;
-    }
+      }
+    });
+    game.activePlayers
+      .find((p) => p.id === targetPlayer.id)
+      .cards.forEach((c) => {
+        game.activePlayers
+          .find((p) => p.id === targetPlayer.id)
+          .discardedCards.push(c);
+      });
+    game.activePlayers.find((p) => p.id === targetPlayer.id).cards = [];
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
-
 
 function king(targetPlayer, currentPlayer, game) {
-    try {
-        let opponentCards = targetPlayer.cards;
-        game.activePlayers.find(p => p.id === targetPlayer.id).cards = currentPlayer.cards;
-        game.activePlayers.find(p => p.id === targetPlayer.id).cards.forEach(c => c.playerId = targetPlayer.id);
-        game.activePlayers.find(p => p.id === currentPlayer.id).cards = opponentCards;
-        game.activePlayers.find(p => p.id === currentPlayer.id).cards.forEach(c => c.playerId = currentPlayer.id);
-        return true;
-    } catch (err) {
-        console.error(err);
-        return false;
-    }
+  try {
+    let opponentCards = targetPlayer.cards;
+    game.activePlayers.find((p) => p.id === targetPlayer.id).cards =
+      currentPlayer.cards;
+    game.activePlayers
+      .find((p) => p.id === targetPlayer.id)
+      .cards.forEach((c) => (c.playerId = targetPlayer.id));
+    game.activePlayers.find(
+      (p) => p.id === currentPlayer.id
+    ).cards = opponentCards;
+    game.activePlayers
+      .find((p) => p.id === currentPlayer.id)
+      .cards.forEach((c) => (c.playerId = currentPlayer.id));
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 }
 
 function countess() {
-    return true;
-}
-
-
-function princess(currentPlayer, game) {
-    try {
-        return score.loose(game, currentPlayer);
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
+  return true;
 }
 
 function princess(currentPlayer, game) {
@@ -139,4 +127,22 @@ function princess(currentPlayer, game) {
   }
 }
 
-module.exports = { guard, priest, baron, handmaid, prince, king, princess, countess }
+function princess(currentPlayer, game) {
+  try {
+    return score.loose(game, currentPlayer);
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
+module.exports = {
+  guard,
+  priest,
+  baron,
+  handmaid,
+  prince,
+  king,
+  princess,
+  countess,
+};
